883ad6afe943dd5bbb08fc586b3c6b33cfcd9fb1
Angela <yaoliu928@gmail.com>
2024-03-17 22:33:44 +1000

    add image and style

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Logo from './dojo-logo.png'
 
 export default function Navbar() {
   return (
     <nav>
+      <Image
+        src={Logo}
+        alt='Logo'
+        width={70}
+        quality={100}
+        placeholder='blur'
+      />
       <h1>Helpdesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,5 +1,7 @@
 export default function Tickets() {
   return (
-    <div>Tickets</div>
+    <main>
+      <h2>Tickets</h2>
+    </main>
   )
 }
