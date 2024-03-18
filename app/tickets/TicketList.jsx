5ae1261c55bb08c570359080a78a71fd573b3ee7
Angela <yaoliu928@gmail.com>
2024-03-18 18:56:53 +1000

    make ticket details page static rendering

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 async function getTickets() {
   const res = await fetch('http://localhost:4000/tickets', {
@@ -19,11 +20,13 @@ export default async function TicketList() {
       {
         tickets.map((ticket) => (
           <div key={ticket.id} className='card my-5'>
-            <h3>{ticket.title}</h3>
-            <p>{ticket.body.slice(0, 200)}...</p>
-            <div className={`pill ${ticket.priority}`}>
-              {ticket.priority} priority
-            </div>
+            <Link href={`/tickets/${ticket.id}`}>
+              <h3>{ticket.title}</h3>
+              <p>{ticket.body.slice(0, 200)}...</p>
+              <div className={`pill ${ticket.priority}`}>
+                {ticket.priority} priority
+              </div>
+            </Link>
           </div>
         ))}
       {tickets.length === 0 && (
---
+++ app/tickets/[id]/page.jsx
@@ -0,0 +1,47 @@
+import { notFound } from 'next/navigation'
+import React from 'react'
+
+export const dynamicParams = true
+
+export async function generateStaticParams() {
+  const res = await fetch(`http://localhost:4000/tickets`)
+
+  const tickets = await res.json()
+
+  return tickets.map((ticket) => ({
+    id: ticket.id
+  }))
+}
+
+async function getTickets(id) {
+  const res = await fetch(`http://localhost:4000/tickets/${id}`, {
+    next: {
+      revalidate: 60
+    }
+  })
+
+  if (!res.ok) {
+    notFound()
+  }
+
+  return res.json()
+}
+
+export default async function TicketDetails({ params }) {
+  const ticket = await getTickets(params.id)
+  return (
+    <main>
+      <nav>
+        <h2>Ticket Details</h2>
+        <div className='card'>
+          <h3>{ticket.title}</h3>
+          <small>Created by {ticket.user_email}</small>
+          <p>{ticket.body}</p>
+          <div className={`pill ${ticket.priority}`}>
+            {ticket.priority} priority
+          </div>
+        </div>
+      </nav>
+    </main>
+  )
+}
