42e264fea992eef6fb82f14b639fba40dacae527
Angela <yaoliu928@gmail.com>
2024-03-19 20:41:40 +1000

    client component

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CreateForm from './CreateForm'
+
+export default function CreateTicket() {
+  return (
+    <main>
+      <h2 className='text-primary text-center'>
+        Add a New Ticket
+      </h2>
+      <CreateForm />
+    </main>
+  )
+}
